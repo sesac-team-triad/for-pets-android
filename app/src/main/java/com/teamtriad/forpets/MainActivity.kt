021524551738bb1384523d37f627df18d8d5282b
JAESEONG-L <dlwotjd116@snu.ac.kr>
2024-01-11 02:02:39 +0900

    feat: Main 액티비티에 Jetpack Navigation 구현 및 BottomNavigationView와 연동
    Co-authored-by: kska01 <insideka01@gmail.com>
    Co-authored-by: Ju-lotty <jjg991201@gmail.com>

--- app/src/main/java/com/teamtriad/forpets/MainActivity.kt
+++ app/src/main/java/com/teamtriad/forpets/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.teamtriad.forpets
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.teamtriad.forpets.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding.bnv.setupWithNavController(navController)
     }
 }
