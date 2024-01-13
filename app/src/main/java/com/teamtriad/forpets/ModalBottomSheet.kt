509d838f6d6cda1a61a45555bbae798b6a5bed3a
kska01 <insideka01@kakao.com>
2024-01-13 22:01:09 +0900

    feat: 봉사요청 등록 화면의 출발지역과 도착지역 입력 버튼 기능 구현

--- app/src/main/java/com/teamtriad/forpets/ModalBottomSheet.kt
+++ app/src/main/java/com/teamtriad/forpets/ModalBottomSheet.kt
@@ -4,15 +4,47 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.teamtriad.forpets.databinding.BottomSheetLocationBinding
+import com.teamtriad.forpets.tmp.Location
 
 class ModalBottomSheet : BottomSheetDialogFragment() {
+
+    private var _binding: BottomSheetLocationBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.bottom_sheet_location, container, false)
+    ): View {
+        _binding = BottomSheetLocationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val counties = Location.loadLocationMap()
+        val adapter = ArrayAdapter(
+            requireContext(),
+            R.layout.dropdown_item,
+            counties.keys.toTypedArray()
+        )
+        with(binding) {
+            actvCounty.setAdapter(adapter)
+            actvCounty.setOnItemClickListener { _, _, _, _ ->
+                val selectedCounty = binding.tilBottomCounty.editText?.text.toString()
+                val cities = counties[selectedCounty]?.toTypedArray()
+                val adapter = ArrayAdapter(
+                    requireContext(),
+                    R.layout.dropdown_item,
+                    cities!!
+                )
+                actvDistrict.setAdapter(adapter)
+            }
+        }
+    }
 
     companion object {
         const val TAG = "ModalBottomSheet"
