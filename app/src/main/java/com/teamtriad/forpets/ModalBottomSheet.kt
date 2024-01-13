5fe75090d9a59dc5e6c631ae2b38abc45ea5f610
kska01 <insideka01@kakao.com>
2024-01-13 18:06:22 +0900

    add: ModalBottomSheetDialog를 위한 xml, class 파일 추가

---
+++ app/src/main/java/com/teamtriad/forpets/ModalBottomSheet.kt
@@ -0,0 +1,20 @@
+package com.teamtriad.forpets
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class ModalBottomSheet : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.bottom_sheet_location, container, false)
+
+    companion object {
+        const val TAG = "ModalBottomSheet"
+    }
+}
