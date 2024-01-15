3a1755990d089d9449191762d498cdd29f7e8d45
JAESEONG-L <dlwotjd116@snu.ac.kr>
2024-01-15 11:32:14 +0900

    feat: 입양 목록 화면의 기능 구현

--- app/src/main/java/com/teamtriad/forpets/AdoptFragment.kt
+++ app/src/main/java/com/teamtriad/forpets/AdoptFragment.kt
@@ -1,18 +1,63 @@
 package com.teamtriad.forpets
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.teamtriad.forpets.adapter.AdoptAdapter
+import com.teamtriad.forpets.databinding.FragmentAdoptBinding
+import com.teamtriad.forpets.model.AbandonmentInfo
+import com.teamtriad.forpets.util.toYyyyMmDd
+import kotlinx.coroutines.launch
+import java.util.Date
 
 class AdoptFragment : Fragment() {
+
+    private var _binding: FragmentAdoptBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_adopt, container, false)
+    ): View {
+        _binding = FragmentAdoptBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launch {
+            binding.root.adapter = AdoptAdapter(requestAbandonmentInfos())
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private suspend fun requestAbandonmentInfos(): List<AbandonmentInfo> {
+        val adoptService: AdoptService = AdoptService.getService()
+
+        val response = adoptService.getAbandonmentPublic(
+            "20240101",
+            Date().toYyyyMmDd(),
+            MAX_NUM_OF_ROWS.toString()
+        )
+
+        return response.body()
+            ?.response
+            ?.body
+            ?.items
+            ?.item ?: listOf()
+    }
+
+    companion object {
+
+        const val MAX_NUM_OF_ROWS: Int = 1000
     }
 }
