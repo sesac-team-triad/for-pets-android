b58e39ea27dff87000d74bfd95234698db33620e
JAESEONG-L <dlwotjd116@snu.ac.kr>
2024-01-16 03:33:26 +0900

    feat: getAbandonmentPublic() 및 필수 항목이 누락된 경우에 대한 예외처리

--- app/src/main/java/com/teamtriad/forpets/model/ResponseHeader.kt
+++ app/src/main/java/com/teamtriad/forpets/model/ResponseHeader.kt
@@ -2,7 +2,7 @@ package com.teamtriad.forpets.model
 
 data class ResponseHeader(
     // 요청 고유번호
-    val reqNo: Int,
+    val reqNo: Int?,
     // 결과코드
     val resultCode: String,
     val resultMsg: String,
