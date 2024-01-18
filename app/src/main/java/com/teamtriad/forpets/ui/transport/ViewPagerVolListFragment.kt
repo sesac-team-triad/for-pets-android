96a3a6178944a57733e759fdd06c668b0ee5b59c
kska01 <insideka01@kakao.com>
2024-01-18 16:54:05 +0900

    feat: 봉사자 목록 화면 recyclerview + expandable cardview 구현

--- app/src/main/java/com/teamtriad/forpets/ui/transport/ViewPagerVolListFragment.kt
+++ app/src/main/java/com/teamtriad/forpets/ui/transport/ViewPagerVolListFragment.kt
@@ -5,15 +5,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.teamtriad.forpets.R
+import com.teamtriad.forpets.databinding.FragmentViewPagerVolListBinding
+import com.teamtriad.forpets.tmp.Volunteers
+import com.teamtriad.forpets.ui.transport.adpater.RecyclerviewVolListAdapter
 
 class ViewPagerVolListFragment : Fragment() {
+
+    private var _binding: FragmentViewPagerVolListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_pager_vol_list, container, false)
+    ): View {
+        _binding = FragmentViewPagerVolListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setRecyclerview()
+    }
+
+    private fun setRecyclerview() {
+        val volList = Volunteers.loadList()
+        val recyclerview = binding.rvVolList
+        val adapter = RecyclerviewVolListAdapter()
+        adapter.submitList(volList)
+        recyclerview.adapter = adapter
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
---
+++ app/src/main/java/com/teamtriad/forpets/ui/transport/adpater/RecyclerviewVolListAdapter.kt
@@ -0,0 +1,76 @@
+package com.teamtriad.forpets.ui.transport.adpater
+
+import android.transition.AutoTransition
+import android.transition.TransitionManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.teamtriad.forpets.databinding.RvItemVolListBinding
+import com.teamtriad.forpets.tmp.Volunteer
+
+class RecyclerviewVolListAdapter :
+    ListAdapter<Volunteer, RecyclerviewVolListAdapter.VolListViewModel>(DiffCallback) {
+
+    private lateinit var binding: RvItemVolListBinding
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VolListViewModel {
+        binding = RvItemVolListBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return VolListViewModel(binding)
+    }
+
+    override fun onBindViewHolder(holder: VolListViewModel, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class VolListViewModel(val binding: RvItemVolListBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            with(binding) {
+                cvVolList.setOnClickListener {
+                    if (clHiddenLayout.visibility == View.VISIBLE) {
+                        TransitionManager.beginDelayedTransition(cvVolList, AutoTransition())
+                        clHiddenLayout.visibility = View.GONE
+                        groupShortenedLocationName.visibility = View.VISIBLE
+                    } else {
+                        TransitionManager.beginDelayedTransition(cvVolList, AutoTransition())
+                        clHiddenLayout.visibility = View.VISIBLE
+                        groupShortenedLocationName.visibility = View.GONE
+                    }
+                }
+            }
+        }
+
+        fun bind(volunteer: Volunteer) {
+            with(binding) {
+                tvName.text = volunteer.name
+                tvCountyFrom.text = volunteer.shortenedLocationFrom
+                tvCountyTo.text = volunteer.shortenedLocationTo
+                tvDate.text = volunteer.date
+                tvDetailLocationFrom.text = volunteer.detailFrom
+                tvDetailLocationTo.text = volunteer.detailTo
+                tvMessage.text = volunteer.message
+            }
+        }
+    }
+
+    companion object {
+        private val DiffCallback = object : DiffUtil.ItemCallback<Volunteer>() {
+            override fun areItemsTheSame(oldItem: Volunteer, newItem: Volunteer): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: Volunteer, newItem: Volunteer): Boolean {
+                return oldItem.message == newItem.message
+            }
+
+        }
+    }
+}
