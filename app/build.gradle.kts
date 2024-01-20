9f06f3c4643eaeb4fcdd6557af33eb7d960bcca3
Ju-lotty <jjg991201@gmail.com>
2024-01-20 20:25:00 +0900

    feedback: 피드백 반영 의존성, 로그인 xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,8 +59,8 @@ dependencies {
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
-    implementation("com.squareup.moshi:moshi:1.12.0")
-    implementation("com.squareup.moshi:moshi-kotlin:1.12.0")
+    implementation("com.squareup.moshi:moshi:1.14.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
 
 
 }
