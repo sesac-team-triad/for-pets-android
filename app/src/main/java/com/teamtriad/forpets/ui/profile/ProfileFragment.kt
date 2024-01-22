04b489aec0925a4756d8392f236f098275745178
JAESEONG-L <dlwotjd116@snu.ac.kr>
2024-01-22 13:36:39 +0900

    feat: 나의 정보 화면에서 봉사요청 목록 화면으로 이동하는 버튼 임시 구현

--- app/src/main/java/com/teamtriad/forpets/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/teamtriad/forpets/ui/profile/ProfileFragment.kt
@@ -1,19 +1,38 @@
 package com.teamtriad.forpets.ui.profile
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.teamtriad.forpets.R
+import com.teamtriad.forpets.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnTransportLists.setOnClickListener {
+            findNavController().navigate(R.id.action_profileFragment_to_transportListsFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
