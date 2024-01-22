3343584a490ddff7ccf6a5aea340b80db9ce0683
JAESEONG-L <dlwotjd116@snu.ac.kr>
2024-01-22 17:36:43 +0900

    feat: AbandonmentPublic 데이터 클래스에 Moshi Codegen 적용

--- app/src/main/java/com/teamtriad/forpets/data/source/network/AbandonmentInfo.kt
+++ app/src/main/java/com/teamtriad/forpets/data/source/network/AbandonmentInfo.kt
@@ -1,7 +1,9 @@
 package com.teamtriad.forpets.data.source.network
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 data class AbandonmentInfo(
     @Json(name = "filename") val thumbnailImageUrl: String,
     @Json(name = "happenDt") val happenDate: String,
--- app/src/main/java/com/teamtriad/forpets/data/source/network/ResponseHeader.kt
+++ app/src/main/java/com/teamtriad/forpets/data/source/network/ResponseHeader.kt
@@ -1,5 +1,8 @@
 package com.teamtriad.forpets.data.source.network
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class ResponseHeader(
     // 요청 고유번호
     val reqNo: Int?,
