7271c96653e7a0685d535eeae04e9d40c31fed4a
kska01 <insideka01@kakao.com>
2024-01-22 17:32:35 +0900

    feat: 이동봉사 화면 구글 지도 및 마커 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,4 +78,5 @@ dependencies {
     implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
 
     implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
 }
--- app/src/main/java/com/teamtriad/forpets/ui/transport/TransportFragment.kt
+++ app/src/main/java/com/teamtriad/forpets/ui/transport/TransportFragment.kt
@@ -6,10 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.teamtriad.forpets.R
 import com.teamtriad.forpets.databinding.FragmentTransportBinding
 
-class TransportFragment : Fragment() {
+class TransportFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentTransportBinding? = null
     private val binding get() = _binding!!
@@ -25,6 +31,14 @@ class TransportFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val mapFragment = SupportMapFragment.newInstance()
+        requireActivity().supportFragmentManager
+            .beginTransaction()
+            .add(R.id.map, mapFragment)
+            .commit()
+        mapFragment.getMapAsync(this)
+
+
         binding.efabTransportReq.setOnClickListener {
             findNavController().navigate(R.id.action_transportFragment_to_transportReqFragment)
         }
@@ -36,5 +50,15 @@ class TransportFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val sesac = LatLng(37.560,  127.064)
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(sesac)
+                .title("sesac")
+        )
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sesac))
     }
 }
