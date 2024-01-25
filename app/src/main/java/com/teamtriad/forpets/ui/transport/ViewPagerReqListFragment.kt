17b51b1b877761b635b286d61caf3ecde6b28e3a
JAESEONG-L <dlwotjd116@snu.ac.kr>
2024-01-26 03:48:53 +0900

    feat: 지역 선택 다이얼로그를 봉사요청 목록 화면에 연결

--- app/src/main/java/com/teamtriad/forpets/ui/transport/ViewPagerReqListFragment.kt
+++ app/src/main/java/com/teamtriad/forpets/ui/transport/ViewPagerReqListFragment.kt
@@ -1,10 +1,13 @@
 package com.teamtriad.forpets.ui.transport
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.teamtriad.forpets.databinding.FragmentViewPagerReqListBinding
 
 class ViewPagerReqListFragment : Fragment() {
@@ -19,6 +22,33 @@ class ViewPagerReqListFragment : Fragment() {
         _binding = FragmentViewPagerReqListBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            tietFrom.setOnClickListener {
+                root.requestFocus()
+
+                navigateToLocationPickerDialog(false)
+            }
+            tietTo.setOnClickListener {
+                root.requestFocus()
+
+                navigateToLocationPickerDialog(false)
+            }
+        }
+    }
+
+    private fun navigateToLocationPickerDialog(onlyCounty: Boolean) {
+        val action =
+            TransportListsFragmentDirections.actionTransportListsFragmentToLocationPickerDialogFragment(
+                onlyCounty
+            )
+
+        findNavController().navigate(action)
     }
 
     override fun onDestroyView() {
