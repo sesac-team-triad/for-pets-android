d19838620762353b0ef3df781882cb216a2fd399
JAESEONG-L <dlwotjd116@snu.ac.kr>
2024-02-05 16:38:13 +0900

    feat: TransportRepository 완성
    Co-authored-by: kska01 <insideka01@gmail.com>

--- app/src/main/java/com/teamtriad/forpets/data/TransportRepository.kt
+++ app/src/main/java/com/teamtriad/forpets/data/TransportRepository.kt
@@ -3,17 +3,15 @@ package com.teamtriad.forpets.data
 import android.util.Log
 import com.teamtriad.forpets.data.source.network.RemoteDatabaseService
 import com.teamtriad.forpets.data.source.network.model.TransportReq
+import com.teamtriad.forpets.data.source.network.model.TransportVol
 
 class TransportRepository(private val databaseService: RemoteDatabaseService) {
 
-    suspend fun addTransportReq(transportReq: TransportReq): Boolean {
-        return try {
+    suspend fun addTransportReq(transportReq: TransportReq) {
+        try {
             databaseService.addTransportReq(transportReq)
-                .isSuccessful
         } catch (e: Exception) {
             Log.e("TransportRepository", e.message.toString())
-
-            false
         }
     }
 
@@ -45,25 +43,57 @@ class TransportRepository(private val databaseService: RemoteDatabaseService) {
         return null
     }
 
-    /*    fun updateTransportReqByKey(key: String, transportReq: TransportReq) {
+    suspend fun updateTransportReqByKey(key: String, transportReq: TransportReq) {
+        try {
+            databaseService.updateTransportReqByKey(key, transportReq)
+        } catch (e: Exception) {
+            Log.e("TransportRepository", e.message.toString())
+        }
+    }
+
+    suspend fun deleteTransportReqByKey(key: String) {
+        try {
+            databaseService.deleteTransportReqByKey(key)
+        } catch (e: Exception) {
+            Log.e("TransportRepository", e.message.toString())
+        }
+    }
+
+    suspend fun addTransportVol(transportVol: TransportVol) {
+        try {
+            databaseService.addTransportVol(transportVol)
+        } catch (e: Exception) {
+            Log.e("TransportRepository", e.message.toString())
+        }
+    }
+
+    suspend fun getAllTransportVolMap(): Map<String, TransportVol>? {
+        try {
+            val response = databaseService.getAllTransportVolMap()
+
+            if (response.isSuccessful) {
+                return response.body()
+            }
+        } catch (e: Exception) {
+            Log.e("TransportRepository", e.message.toString())
         }
 
-        fun deleteTransportReqByKey(key: String) {
-        }
+        return null
+    }
 
-        fun addTransportVol(transportVol: TransportVol) {
+    suspend fun updateTransportVolByKey(key: String, transportVol: TransportVol) {
+        try {
+            databaseService.updateTransportVolByKey(key, transportVol)
+        } catch (e: Exception) {
+            Log.e("TransportRepository", e.message.toString())
         }
+    }
 
-        fun getAllTransportVolMap(): Map<String, TransportVol> {
+    suspend fun deleteTransportVolByKey(key: String) {
+        try {
+            databaseService.deleteTransportVolByKey(key)
+        } catch (e: Exception) {
+            Log.e("TransportRepository", e.message.toString())
         }
-
-        fun getTransportVolByKey(key: String): TransportVol {
-        }
-
-        fun updateTransportVolByKey(key: String, transportVol: TransportVol) {
-        }
-
-        fun deleteTransportVolByKey(key: String) {
-        }
-        */
+    }
 }
--- app/src/main/java/com/teamtriad/forpets/data/source/network/RemoteDatabaseService.kt
+++ app/src/main/java/com/teamtriad/forpets/data/source/network/RemoteDatabaseService.kt
@@ -1,11 +1,13 @@
 package com.teamtriad.forpets.data.source.network
 
-import com.teamtriad.forpets.data.source.network.model.KeyResponse
 import com.teamtriad.forpets.data.source.network.model.TransportReq
+import com.teamtriad.forpets.data.source.network.model.TransportVol
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface RemoteDatabaseService {
@@ -13,7 +15,7 @@ interface RemoteDatabaseService {
     @POST("transport-req.json")
     suspend fun addTransportReq(
         @Body transportReq: TransportReq
-    ): Response<KeyResponse>
+    ): Response<Unit>
 
     @GET("transport-req.json")
     suspend fun getAllTransportReqMap(): Response<Map<String, TransportReq>>
@@ -22,4 +24,34 @@ interface RemoteDatabaseService {
     suspend fun getTransportReqByKey(
         @Path("key") key: String
     ): Response<TransportReq>
+
+    @PUT("transport-req/{key}.json")
+    suspend fun updateTransportReqByKey(
+        @Path("key") key: String,
+        @Body transportReq: TransportReq
+    ): Response<Unit>
+
+    @DELETE("transport-req/{key}.json")
+    suspend fun deleteTransportReqByKey(
+        @Path("key") key: String
+    ): Response<Unit>
+
+    @POST("transport-vol.json")
+    suspend fun addTransportVol(
+        @Body transportVol: TransportVol
+    ): Response<Unit>
+
+    @GET("transport-vol.json")
+    suspend fun getAllTransportVolMap(): Response<Map<String, TransportVol>>
+
+    @PUT("transport-vol/{key}.json")
+    suspend fun updateTransportVolByKey(
+        @Path("key") key: String,
+        @Body transportVol: TransportVol
+    ): Response<Unit>
+
+    @DELETE("transport-vol/{key}.json")
+    suspend fun deleteTransportVolByKey(
+        @Path("key") key: String
+    ): Response<Unit>
 }
