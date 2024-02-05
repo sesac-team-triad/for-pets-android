08287eee05d203fc1ce60d83b96808183ea66736
JAESEONG-L <dlwotjd116@snu.ac.kr>
2024-02-05 22:39:08 +0900

    feat: UserRepository 구현
    Co-authored-by: kska01 <insideka01@gmail.com>
    Co-authored-by: Ju-lotty <jjg991201@gmail.com>

---
+++ app/src/main/java/com/teamtriad/forpets/data/UserRepository.kt
@@ -0,0 +1,60 @@
+package com.teamtriad.forpets.data
+
+import android.util.Log
+import com.teamtriad.forpets.data.source.network.RemoteDatabaseService
+import com.teamtriad.forpets.data.source.network.model.User
+
+class UserRepository(private val databaseService: RemoteDatabaseService) {
+
+    suspend fun addUserByUid(uid: String, user: User) {
+        try {
+            databaseService.addUserByUid(uid, user)
+        } catch (e: Exception) {
+            Log.e("UserRepository", e.message.toString())
+        }
+    }
+
+    suspend fun getUserByUid(uid: String): User? {
+        try {
+            val response = databaseService.getUserByUid(uid)
+
+            if (response.isSuccessful) {
+                return response.body()
+            }
+        } catch (e: Exception) {
+            Log.e("UserRepository", e.message.toString())
+        }
+
+        return null
+    }
+
+    suspend fun getUserNicknameByUid(uid: String): String? {
+        try {
+            val response = databaseService.getUserNicknameByUid(uid)
+
+            if (response.isSuccessful) {
+                return response.body()
+            }
+        } catch (e: Exception) {
+            Log.e("UserRepository", e.message.toString())
+        }
+
+        return null
+    }
+
+    suspend fun updateUserByUid(uid: String, user: User) {
+        try {
+            databaseService.updateUserByUid(uid, user)
+        } catch (e: Exception) {
+            Log.e("UserRepository", e.message.toString())
+        }
+    }
+
+    suspend fun deleteUserByUid(uid: String) {
+        try {
+            databaseService.deleteUserByUid(uid)
+        } catch (e: Exception) {
+            Log.e("UserRepository", e.message.toString())
+        }
+    }
+}
