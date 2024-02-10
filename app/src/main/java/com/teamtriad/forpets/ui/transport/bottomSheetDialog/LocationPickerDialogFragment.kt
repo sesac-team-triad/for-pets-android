ff06b5c2a7575e0644c798543ff8d29663655acc
kska01 <insideka01@kakao.com>
2024-02-10 13:53:32 +0900

    feat: 다이얼로그에서 입력버튼이 클릭될 경우 다이얼로그가 닫히는 기능 추가

--- app/src/main/java/com/teamtriad/forpets/ui/transport/bottomSheetDialog/LocationPickerDialogFragment.kt
+++ app/src/main/java/com/teamtriad/forpets/ui/transport/bottomSheetDialog/LocationPickerDialogFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.teamtriad.forpets.R
@@ -60,7 +61,7 @@ class LocationPickerDialogFragment : BottomSheetDialogFragment() {
                 transportViewModel.setSelectedToDistrict(selectedDistrict)
             }
 
-            dismiss()
+            findNavController().popBackStack()
         }
     }
 
