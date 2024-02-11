6a369053c3923108fa448358a1f1ffe8c3dbae34
JAESEONG-L <dlwotjd116@snu.ac.kr>
2024-02-11 13:06:49 +0900

    add: TransportMovingListFragment.kt 파일 생성

---
+++ app/src/main/java/com/teamtriad/forpets/ui/transport/TransportMovingListFragment.kt
@@ -0,0 +1,16 @@
+package com.teamtriad.forpets.ui.transport
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.teamtriad.forpets.R
+
+class TransportMovingListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_transport_moving_list, container, false)
+}
