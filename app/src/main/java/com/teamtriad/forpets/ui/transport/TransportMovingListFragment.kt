549e3841b222693f4ad6d4cf4cea8628e961f99d
JAESEONG-L <dlwotjd116@snu.ac.kr>
2024-02-13 15:10:31 +0900

    feat: 이동중 목록 프래그먼트 구현

--- app/src/main/java/com/teamtriad/forpets/ui/transport/TransportMovingListFragment.kt
+++ app/src/main/java/com/teamtriad/forpets/ui/transport/TransportMovingListFragment.kt
@@ -5,12 +5,59 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.teamtriad.forpets.R
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.teamtriad.forpets.databinding.FragmentTransportMovingListBinding
+import com.teamtriad.forpets.ui.transport.adapter.MovingListRecyclerViewAdapter
+import com.teamtriad.forpets.viewmodel.ProfileViewModel
+import com.teamtriad.forpets.viewmodel.TransportViewModel
+import kotlinx.coroutines.launch
 
 class TransportMovingListFragment : Fragment() {
+
+    private val transportViewModel: TransportViewModel by activityViewModels()
+    private val profileViewModel: ProfileViewModel by viewModels()
+
+    private var _binding: FragmentTransportMovingListBinding? = null
+    private val binding get() = _binding!!
+
+    private val recyclerViewAdapter by lazy {
+        MovingListRecyclerViewAdapter(lifecycleScope, transportViewModel, profileViewModel)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_transport_moving_list, container, false)
+    ): View {
+        _binding = FragmentTransportMovingListBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launch {
+            transportViewModel.getAllMovingMap()
+                .join()
+
+            binding.setRecyclerView()
+        }
+    }
+
+    private fun FragmentTransportMovingListBinding.setRecyclerView() {
+        root.adapter = recyclerViewAdapter.apply {
+            transportViewModel.movingMap.observe(viewLifecycleOwner) {
+                submitList(
+                    it.values.toList()
+                )
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
