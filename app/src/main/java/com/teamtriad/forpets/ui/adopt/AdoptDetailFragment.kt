416978b992d87ccf741eb4b7f2d50e9a0562354e
JAESEONG-L <dlwotjd116@snu.ac.kr>
2024-02-18 03:29:38 +0900

    feat: args.index를 이용하는 입양 상세화면 구현

--- app/src/main/java/com/teamtriad/forpets/ui/adopt/AdoptDetailFragment.kt
+++ app/src/main/java/com/teamtriad/forpets/ui/adopt/AdoptDetailFragment.kt
@@ -1,18 +1,24 @@
 package com.teamtriad.forpets.ui.adopt
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
+import com.teamtriad.forpets.data.source.network.model.AbandonmentInfo
 import com.teamtriad.forpets.databinding.FragmentAdoptDetailBinding
 import com.teamtriad.forpets.util.glide
+import com.teamtriad.forpets.viewmodel.AdoptViewModel
 
 class AdoptDetailFragment : Fragment() {
 
     private val args: AdoptDetailFragmentArgs by navArgs()
+
+    private val adoptViewModel: AdoptViewModel by activityViewModels()
 
     private var _binding: FragmentAdoptDetailBinding? = null
     private val binding get() = _binding!!
@@ -30,16 +36,53 @@ class AdoptDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        with(binding) { // TODO: ViewModel을 사용하여 데이터를 전달받기.
-            ivImage.glide(args.imageUrl)
+        val abandonmentInfo: AbandonmentInfo = adoptViewModel.abandonmentInfoList[args.index]
+
+        with(binding) {
+            ivImage.glide(abandonmentInfo.imageUrl)
+
+            tvKind.text = "${tvKind.text} ${abandonmentInfo.kind}"
+            tvSex.text = "${tvSex.text} ${abandonmentInfo.sex}"
+            tvDate.text = "${tvDate.text} ${abandonmentInfo.happenDate}"
+
+            tvAge.text = "${tvAge.text} ${abandonmentInfo.age}"
+            tvColor.text = "${tvColor.text} ${abandonmentInfo.color}"
+            tvWeight.text = "${tvWeight.text} ${abandonmentInfo.weight}"
+            tvState.text = "${tvState.text} ${abandonmentInfo.processState}"
+            tvNeuter.text = "${tvNeuter.text} ${abandonmentInfo.neuterYn}"
+            tvSpecialMark.text = when (abandonmentInfo.noticeComment.isNullOrBlank()) {
+                true -> abandonmentInfo.specialMark
+                false -> abandonmentInfo.specialMark + "\n\n" + abandonmentInfo.noticeComment
+            }
+
+            tvCareNm.text = "${tvCareNm.text} ${abandonmentInfo.careNm}"
+            tvCareTel.text = "${tvCareTel.text} ${abandonmentInfo.careTel}"
+            tvCareAddr.text = "${tvCareAddr.text} ${abandonmentInfo.careAddr}"
+            tvChargeNm.text = "${tvChargeNm.text} ${abandonmentInfo.chargeNm}"
+            tvOfficeTel.text = "${tvOfficeTel.text} ${abandonmentInfo.officetel}"
+
+            tvHappenPlace.text = "${tvHappenPlace.text} ${abandonmentInfo.happenPlace}"
+            tvNoticeNo.text = "${tvNoticeNo.text} ${abandonmentInfo.noticeNo}"
 
             btnCare.setOnClickListener {
-                val intent = Intent(Intent.ACTION_DIAL)
+                val intent = Intent(
+                    Intent.ACTION_DIAL, Uri.fromParts(
+                        "tel",
+                        abandonmentInfo.careTel,
+                        null
+                    )
+                )
 
                 startActivity(intent)
             }
             btnCharge.setOnClickListener {
-                val intent = Intent(Intent.ACTION_DIAL)
+                val intent = Intent(
+                    Intent.ACTION_DIAL, Uri.fromParts(
+                        "tel",
+                        abandonmentInfo.officetel,
+                        null
+                    )
+                )
 
                 startActivity(intent)
             }
