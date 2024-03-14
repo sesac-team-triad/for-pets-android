a01dce0d5af51d613873b64fe1757eb17ea193d5
kska01 <insideka01@gmail.com>
2024-03-14 14:59:06 +0900

    feat: 이메일 입력시 이메일 주소 유효성 검사하는 기능 구현

--- app/src/main/java/com/teamtriad/forpets/ui/login/SignUpIndividualFragment.kt
+++ app/src/main/java/com/teamtriad/forpets/ui/login/SignUpIndividualFragment.kt
@@ -1,10 +1,13 @@
 package com.teamtriad.forpets.ui.login
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.teamtriad.forpets.R
 import com.teamtriad.forpets.databinding.FragmentSignUpIndividualBinding
 
 class SignUpIndividualFragment : Fragment() {
@@ -18,6 +21,36 @@ class SignUpIndividualFragment : Fragment() {
     ): View {
         _binding = FragmentSignUpIndividualBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setOnClickListener()
+        checkEmailAddress()
+
+    }
+
+    private fun setOnClickListener() = with(binding) {
+    }
+
+    private fun checkEmailAddress() {
+        binding.tietEmail.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                val isValid = android.util.Patterns.EMAIL_ADDRESS.matcher(s.toString()).matches()
+
+                if (!isValid) {
+                    binding.tilEmail.error = getString(R.string.sign_up_email_error_message)
+                } else {
+                    binding.tilEmail.error = null
+                }
+            }
+        })
     }
 
     override fun onDestroyView() {
