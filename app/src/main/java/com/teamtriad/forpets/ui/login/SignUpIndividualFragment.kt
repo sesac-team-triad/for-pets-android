0982c33db2dab3d57eaf0d32db477e33e709c3dd
kska01 <insideka01@gmail.com>
2024-03-14 17:55:09 +0900

    feat: 이메일 주소 입력 후 인증번호 전송버튼 활성화 및 스낵바 기능 구현 그리고 사용자 이메일로 인증번호 전송하는 기능 구현

--- app/src/main/java/com/teamtriad/forpets/ui/login/SignUpIndividualFragment.kt
+++ app/src/main/java/com/teamtriad/forpets/ui/login/SignUpIndividualFragment.kt
@@ -1,19 +1,37 @@
 package com.teamtriad.forpets.ui.login
 
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
+import com.teamtriad.forpets.BuildConfig.EMAIL_ADDRESS
+import com.teamtriad.forpets.BuildConfig.EMAIL_PASSOWRD
 import com.teamtriad.forpets.R
 import com.teamtriad.forpets.databinding.FragmentSignUpIndividualBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.Properties
+import javax.mail.Message
+import javax.mail.PasswordAuthentication
+import javax.mail.Session
+import javax.mail.Transport
+import javax.mail.internet.InternetAddress
+import javax.mail.internet.MimeMessage
 
 class SignUpIndividualFragment : Fragment() {
 
     private var _binding: FragmentSignUpIndividualBinding? = null
     private val binding get() = _binding!!
+
+    private var isValidEmail = false
+    private lateinit var authCode: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,10 +49,60 @@ class SignUpIndividualFragment : Fragment() {
     }
 
     private fun setOnClickListener() = with(binding) {
+        btnSendAuthCode.setOnClickListener {
+            if (isValidEmail) {
+                authCode = sendEmail(tietEmail.text.toString())
+
+                val softKeyboard =
+                    requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                softKeyboard.hideSoftInputFromWindow(root.windowToken, 0)
+
+                Snackbar.make(
+                    requireView(),
+                    R.string.sign_up_email_transport_complete,
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
 
-    private fun checkEmailAddress() {
-        binding.tietEmail.addTextChangedListener(object : TextWatcher {
+    private fun sendEmail(userEmail: String): String {
+        val email = EMAIL_ADDRESS
+        val password = EMAIL_PASSOWRD
+        val code = (100..10000).random().toString()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val props = Properties()
+            props.setProperty("mail.transport.protocol", "smtp")
+            props.setProperty("mail.host", "smtp.gmail.com")
+            props.put("mail.smtp.auth", "true")
+            props.put("mail.smtp.port", "465")
+            props.put("mail.smtp.socketFactory.port", "465")
+            props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory")
+            props.put("mail.smtp.socketFactory.fallback", "false")
+            props.put("mail.smtp.ssl.enable", "true")
+            props.setProperty("mail.smtp.quitwait", "false")
+
+            val session = Session.getInstance(props,
+                object : javax.mail.Authenticator() {
+                    override fun getPasswordAuthentication(): PasswordAuthentication {
+                        return PasswordAuthentication(email, password)
+                    }
+                })
+
+            val message = MimeMessage(session)
+            message.sender = InternetAddress(email)
+            message.addRecipient(Message.RecipientType.TO, InternetAddress(userEmail))
+            message.subject = getString(R.string.sign_up_email_title)
+            message.setText(getString(R.string.sign_up_email_message, code))
+
+            Transport.send(message)
+        }
+        return code
+    }
+
+    private fun checkEmailAddress() = with(binding) {
+        tietEmail.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
 
@@ -42,12 +110,20 @@ class SignUpIndividualFragment : Fragment() {
             }
 
             override fun afterTextChanged(s: Editable?) {
-                val isValid = android.util.Patterns.EMAIL_ADDRESS.matcher(s.toString()).matches()
+                isValidEmail = android.util.Patterns.EMAIL_ADDRESS.matcher(s.toString()).matches()
 
-                if (!isValid) {
-                    binding.tilEmail.error = getString(R.string.sign_up_email_error_message)
+                if (isValidEmail) {
+                    tilEmail.error = null
+                    btnSendAuthCode.apply {
+                        isEnabled = true
+                        isClickable = true
+                    }
                 } else {
-                    binding.tilEmail.error = null
+                    tilEmail.error = getString(R.string.sign_up_email_error_message)
+                    btnSendAuthCode.apply {
+                        isEnabled = false
+                        isClickable = false
+                    }
                 }
             }
         })
