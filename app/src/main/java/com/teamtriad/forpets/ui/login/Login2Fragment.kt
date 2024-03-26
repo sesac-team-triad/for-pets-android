c93fc1abdc169d7d8978ab697e4a5823e7e76a67
kska01 <insideka01@gmail.com>
2024-03-26 15:25:57 +0900

    feat: '로그인' 버튼 활성화 및 비활성화 기능 구현

--- app/src/main/java/com/teamtriad/forpets/ui/login/Login2Fragment.kt
+++ app/src/main/java/com/teamtriad/forpets/ui/login/Login2Fragment.kt
@@ -2,6 +2,8 @@ package com.teamtriad.forpets.ui.login
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -27,12 +29,13 @@ class Login2Fragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setOnClickListeners()
+        checkButtonEnabled()
     }
 
     private fun setOnClickListeners() {
         with(binding) {
-            root.setOnClickListener {
-                root.clearFocus()
+            layout.setOnClickListener {
+                layout.clearFocus()
                 val softKeyboard =
                     requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
                 softKeyboard.hideSoftInputFromWindow(root.windowToken, 0)
@@ -41,6 +44,34 @@ class Login2Fragment : Fragment() {
             tvSignup1.setOnClickListener {
                 findNavController().navigate(R.id.action_login2Fragment_to_signUpIndividualFragment)
             }
+        }
+    }
+
+    private fun checkButtonEnabled() {
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                checkFieldsAndEnableButton()
+            }
+        }
+
+        with(binding) {
+            tietEmail.addTextChangedListener(textWatcher)
+            tietPassword.addTextChangedListener(textWatcher)
+        }
+    }
+
+    private fun checkFieldsAndEnableButton() = with(binding) {
+        val allFieldsFilled = !tietEmail.text.isNullOrEmpty() && !tietPassword.text.isNullOrEmpty()
+
+        btnLogin.apply {
+            isEnabled = allFieldsFilled
+            isClickable = allFieldsFilled
         }
     }
 
