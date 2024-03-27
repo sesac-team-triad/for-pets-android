61fb7b95219cc01876d58cfeac9d48c9d039bdf0
kska01 <insideka01@gmail.com>
2024-03-27 22:57:17 +0900

    feat: 로그인 기능 구현 - 단순히 로그인만

--- app/src/main/java/com/teamtriad/forpets/ui/login/Login2Fragment.kt
+++ app/src/main/java/com/teamtriad/forpets/ui/login/Login2Fragment.kt
@@ -10,10 +10,17 @@ import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.teamtriad.forpets.R
 import com.teamtriad.forpets.databinding.FragmentLogin2Binding
+import com.teamtriad.forpets.util.setSafeOnClickListener
 
 class Login2Fragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
 
     private var _binding: FragmentLogin2Binding? = null
     private val binding get() = _binding!!
@@ -34,15 +41,38 @@ class Login2Fragment : Fragment() {
 
     private fun setOnClickListeners() {
         with(binding) {
-            layout.setOnClickListener {
+            layout.setSafeOnClickListener {
                 layout.clearFocus()
                 val softKeyboard =
                     requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
                 softKeyboard.hideSoftInputFromWindow(root.windowToken, 0)
             }
 
-            tvSignup1.setOnClickListener {
+            tvSignup1.setSafeOnClickListener {
                 findNavController().navigate(R.id.action_login2Fragment_to_signUpIndividualFragment)
+            }
+
+            btnLogin.setSafeOnClickListener {
+                auth = Firebase.auth
+                val userEmail = binding.tietEmail.text.toString()
+                val password = binding.tietPassword.text.toString()
+
+                auth.signInWithEmailAndPassword(userEmail, password)
+                    .addOnCompleteListener(requireActivity()) { task ->
+                        if (task.isSuccessful) {
+                            val user = auth.currentUser
+                            user?.let {
+                                val name = it.displayName
+                                val email = it.email
+                            }
+                        } else {
+                            Snackbar.make(
+                                requireView(),
+                                R.string.login_snack_bar_login_fail,
+                                Snackbar.LENGTH_LONG
+                            ).show()
+                        }
+                    }
             }
         }
     }
